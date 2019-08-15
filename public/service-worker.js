27fc315065d9fbc382ca91e854e4ab3a02091ea9
ecki-d <support@kaizenmedia.co.za>
2019-08-15 18:29:16 +0200

    :service-worker.js:

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,16 @@
+workbox.core.setCacheNameDetails({ prefix: "Chekt" });
+
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 self.addEventListener("push", e => {
-  const data = e.data.json();
-  self.registration.showNotification(`Remember: ${data.title}`, {
-    body: "from Chekt",
-    icon: "/img/icons/apple-icon-120x120.png"
-  });
+  const data = e.data.json() || "";
+
+  if (data.title) {
+    self.registration.showNotification(`Remember: ${data.title}`, {
+      body: "from Chekt",
+      icon: "/img/icons/apple-icon-120x120.png"
+    });
+  }
 });
